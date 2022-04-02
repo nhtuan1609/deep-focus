c3e6aaa9f983bce6444995952ecf88bda98fcca3
nhtuan1609 <nhtuan1609@gmail.com>
2022-04-03 00:44:43 +0700

    update add tooltip and sound item

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,42 @@
   <div>
     <div class="sound__list selected">
       <v-hover v-for="(sound, index) in selectedSounds" v-slot="{ hover }" :key="index">
-        <v-card
-          light
-          :elevation="hover ? 6 : 1"
-          :class="['sound__item', { 'on-hover': hover }]"
-          @click="removeSound(sound)"
-        >
-          <v-img :src="sound.imageSrc" :alt="sound.name" :max-width="50"></v-img>
-        </v-card>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-card
+              light
+              :elevation="hover ? 6 : 1"
+              :class="['sound__item', { 'on-hover': hover }]"
+              v-bind="attrs"
+              v-on="on"
+              @click="removeSound(sound)"
+            >
+              <v-img :src="sound.imageSrc" :alt="sound.name" :max-width="50"></v-img>
+            </v-card>
+          </template>
+          <span>{{ sound.name }}</span>
+        </v-tooltip>
       </v-hover>
     </div>
 
     <div class="sound__list">
       <v-hover v-for="(sound, index) in sounds" v-slot="{ hover }" :key="index">
-        <v-card
-          light
-          :elevation="hover ? 6 : 1"
-          :class="['sound__item', { 'on-hover': hover }]"
-          :disabled="!!selectedSounds.find((item) => item.id === sound.id)"
-          @click="addSound(sound)"
-        >
-          <v-img :src="sound.imageSrc" :alt="sound.name" :max-width="200"></v-img>
-        </v-card>
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-card
+              light
+              :elevation="hover ? 6 : 1"
+              :class="['sound__item', { 'on-hover': hover }]"
+              :disabled="!!selectedSounds.find((item) => item.id === sound.id)"
+              v-bind="attrs"
+              v-on="on"
+              @click="addSound(sound)"
+            >
+              <v-img :src="sound.imageSrc" :alt="sound.name" :max-width="100"></v-img>
+            </v-card>
+          </template>
+          <span>{{ sound.name }}</span>
+        </v-tooltip>
       </v-hover>
     </div>
   </div>
@@ -39,56 +53,91 @@ export default {
   },
   computed: {
     sounds() {
-      return [
+      const soundList = [
         {
-          id: '1',
-          name: 'Birds',
-          imageSrc: require('~/assets/images/birds.png'),
-          soundSrc: ''
-        },
-        {
-          id: '2',
           name: 'Bonfire',
           imageSrc: require('~/assets/images/bonfire.png'),
           soundSrc: ''
         },
         {
-          id: '3',
           name: 'Ocean Waves',
           imageSrc: require('~/assets/images/ocean-waves.png'),
           soundSrc: ''
         },
         {
-          id: '4',
           name: 'Heavy Rain',
           imageSrc: require('~/assets/images/heavy-rain.png'),
           soundSrc: ''
         },
         {
-          id: '5',
+          name: 'Thunder',
+          imageSrc: require('~/assets/images/thunder.png'),
+          soundSrc: ''
+        },
+        {
           name: 'Wind',
           imageSrc: require('~/assets/images/wind.png'),
           soundSrc: ''
         },
         {
-          id: '6',
           name: 'Waterfall',
           imageSrc: require('~/assets/images/waterfall.png'),
           soundSrc: ''
         },
         {
-          id: '7',
+          name: 'Birds',
+          imageSrc: require('~/assets/images/birds.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Frogs',
+          imageSrc: require('~/assets/images/frogs.png'),
+          soundSrc: ''
+        },
+        {
           name: 'Chimes',
           imageSrc: require('~/assets/images/chimes.png'),
           soundSrc: ''
         },
         {
-          id: '8',
-          name: 'Frogs',
-          imageSrc: require('~/assets/images/frogs.png'),
+          name: 'Sleeping',
+          imageSrc: require('~/assets/images/sleeping.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Coffee House',
+          imageSrc: require('~/assets/images/coffee-house.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Cooking',
+          imageSrc: require('~/assets/images/cooking.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Market',
+          imageSrc: require('~/assets/images/market.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Piano',
+          imageSrc: require('~/assets/images/piano.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Yoga',
+          imageSrc: require('~/assets/images/yoga.png'),
+          soundSrc: ''
+        },
+        {
+          name: 'Coding',
+          imageSrc: require('~/assets/images/coding.png'),
           soundSrc: ''
         }
       ]
+      return soundList.map((sound, index) => {
+        return { ...sound, id: index }
+      })
     }
   },
   methods: {
@@ -115,7 +164,7 @@ export default {
     &.selected {
       min-height: 80px;
       gap: 16px;
-      margin-bottom: 64px;
+      margin-bottom: 100px;
     }
   }
 
